51d4446e8c63f180b8d16ea13c027621f237372a
Sara Ebrahimi <94298181+sarEbrahimi@users.noreply.github.com>
2023-07-12 09:17:45 +0330

    Update lbph.py

--- lbph.py
+++ lbph.py
@@ -1,14 +1,26 @@
-import cv2
-from skimage.feature import local_binary_pattern
 import numpy as np
-from scipy.stats import chisquare , itemfreq
+from skimage import color
+import skimage.feature as ft
 
-def calc_lbp(img):
-    # calculate lbp
-    image = local_binary_pattern(img, 16, 8, method='uniform')
-    hist =cv2.calcHist([np.float32(image)] , [0],None,[256],[0,256])
-    #chi = chisquare(image)
+class Comparator:
+    def __init__(self,data,query):
+        self.data = data
+        self.query = query
 
-    return hist
-
-
+    def lbp(self):
+        score = np.inf
+        img = color.rgb2gray(self.query)
+        patterns = ft.local_binary_pattern(img,8,2)
+        n_bins = int( patterns.max()+1 )
+        inputLBPHist, _ = np.histogram(patterns , bins=n_bins, range=(0,n_bins), density=True)
+        for j in self.data:
+            T = 0
+            grayImg = color.rgb2gray(j)
+            lbpPatterns = ft.local_binary_pattern(grayImg,8,2)
+            n_bins = int(lbpPatterns.max() + 1)
+            lbpHist, _ = np.histogram(lbpPatterns, bins=n_bins, range=(0,n_bins), density=True)
+            T = 0.5*np.sum((inputLBPHist-lbpHist)**2/(inputLBPHist+lbpHist+1e-6)) #chi_square
+            if (T<=score):
+                score = T
+                outputImage = j
+        return outputImage
